352d75bb460c90b478785ffc7583bcf0d30bf0a7
Hugo Branco <hjbranco@gmail.com>
2013-04-26 15:01:04 +0100

    Teste

---
+++ githubteste/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githubteste::Application.config.secret_token = '9751ec4890a340168e08ab8afdc562bcc10dc88c86a1119d9c57cefdb32cad6a8aaad77898ecb2bb6b44e4ecfea491fdf024ae2f7ec474fcd7ba8ed9bcc9c1c6'
